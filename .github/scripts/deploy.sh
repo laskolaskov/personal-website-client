65d4ca8766141e832d8e00ffe9a6373906995279
Lasko Laskov <laskov.lasko@gmail.com>
2022-01-08 19:03:48 +0200

    wf changes

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-npm ci
-npm run build
-node ./node_modules/gh-pages/bin/gh-pages.js -d build -r https://${GITHUB_ACTOR}:${token}@github.com/${GITHUB_REPOSITORY}.git > /dev/null
+if [ -n "$1" ]
+then
+    npm ci
+    npm run build
+    node ./node_modules/gh-pages/bin/gh-pages.js -d build -r https://${GITHUB_ACTOR}:$1@github.com/${GITHUB_REPOSITORY}.git > /dev/null
+else
+    echo 'Please provide the github auth token !'
+fi
