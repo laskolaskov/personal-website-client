ab3b918b0dcc4253991e48608862ac76f9d5dc93
Lasko Laskov <laskov.lasko@gmail.com>
2022-01-12 19:44:13 +0200

    wf+deploy script refactor

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-if [ -n "$1" ]
-then
-    npm ci
-    npm run build
-    node ./node_modules/gh-pages/bin/gh-pages.js -d build -r https://${GITHUB_ACTOR}:$1@github.com/${GITHUB_REPOSITORY}.git > /dev/null
-else
-    echo 'Please provide the github auth token !'
-fi
+npm ci
+npm run build
+npm run deploy -- -r https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git > /dev/null
+echo "gh-pages activated !"
